797a6583b8acdc573b45214e0d04cbe682edfb2a
Prabhat <prabhatsaini070@gmail.com>
2021-09-17 00:03:55 +0530

    complatee binnary search

--- Array/binarysearch.c
+++ Array/binarysearch.c
@@ -1,11 +1,81 @@
-#include<stdio.h>
-int main(){
-int arr[5],middle;
-for (int  i = 0; i < 5; i++)
+#include <stdio.h>
+void bubblesort(int *A, int n)
 {
-    scanf("%d",&arr[i]);
+
+    int temp;
+    int sorted;
+    for (int i = 0; i < n - 1; i++)
+
+    {
+        //printf("number pof passes are:%d\n", i + 1);
+        sorted = 1;
+
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+                sorted = 0;
+            }
+        }
+        if (sorted)
+        {
+            return;
+        }
+    }
 }
-middle=(0+5)/2+7;
-printf("%d",middle);
- return 0;
+void printarray(int *A, int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+}
+int main()
+{
+    int n;
+    printf("Ente the lenght of array\n");
+    scanf("%d", &n);
+    int arr[n];
+    int left = 0, lenght = sizeof(arr) / sizeof(arr[0]), right = lenght - 1;
+    printf("Enter the element of array\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    bubblesort(arr, n);
+
+     int searching,flag=0;
+     printf("Enter the number which you want to search\n");
+     scanf("%d",&searching);
+          // loop for binary search 
+   
+     while (left<=right)
+     {
+           int middle = (left + right)/2;
+         if (middle==searching)
+         {
+            flag++;
+               break;
+         }
+         else if (searching<middle)
+         {
+            right=middle-1;
+         }
+         else
+         left=middle+1;
+         
+         
+     }
+     if (flag)
+     {
+         printf("found");
+     }
+     else
+     printf("not found");
+    
+    return 0;
 }
