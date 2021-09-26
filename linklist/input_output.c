d3a09d6c7cc74b032e38c178a72767cbb432e5ea
Prabhat <prabhatsaini070@gmail.com>
2021-09-26 18:51:39 +0530

    complate the code linklist input out

--- linklist/input_output.c
+++ linklist/input_output.c
@@ -1,12 +1,59 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
     int data;
     struct node *next;
 };
-typedef struct node N;
-int main(){
-N *head = NULL;
-printf
- return 0;
+typedef struct node N; 
+N *input(int n)
+{
+    N *head = NULL;
+    N *temp = NULL;
+    N *ptr = NULL;
+    for (int i = 0; i < n; i++)
+    {
+       
+       //indivisual node
+        temp = (N *)malloc(sizeof(N));
+        printf("Enter the element of linklist %d\n", i+1);
+        scanf("%d", &(temp->data));
+        temp->next = NULL;
+        if (head == NULL)
+        {
+            head = temp;
+        }
+        else
+        {
+            ptr = head;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
+               
+            }
+             ptr->next = temp;
+        }
+    }
+    return head;
+}
+void print(N *head)
+{
+    N *p;
+    p=head;
+    while (p!=NULL)
+    {
+        printf("%d->",(p->data));
+        p=p->next;
+    }
+    
+}
+int main()
+{
+    N *Head = NULL;
+    int n;
+    printf("Enter the number of nodes\n");
+    scanf("%d", &n);
+    Head=input(n);
+    print(Head);
+    return 0;
 }
